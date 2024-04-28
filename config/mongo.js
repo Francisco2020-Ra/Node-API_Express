996b9164b86f9ef45f09be1469f9b4f116db32d7
Francisco2020-Ra <franciscorafaelromero@gmail.com>
2024-04-28 16:04:29 -0300

    agregando conexion db mongo

--- config/mongo.js
+++ config/mongo.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose")
 const dbConnect = () =>{
     const DB_URI = process.env.DB_URI;
     mongoose.connect(DB_URI)
+    .then(() => console.log('*** CONEXION EXITOSA ***'))
     .catch( error => console.log("ha ocurrido un error", error));
 };
 
